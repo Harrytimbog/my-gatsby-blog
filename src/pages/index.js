af4bb1586e11c76db0fd51bc4557ce12023c85e0
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-26 16:13:11 +0100

    Add Features with Plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>This is my Blog built with Gatsby!!!</p>
+      <StaticImage
+        alt="The city of Rio de Janeiro"
+        src="../images/profile.jpg"
+      />
     </Layout>
   );
 };
